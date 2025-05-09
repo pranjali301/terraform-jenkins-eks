1e472a9082e1792cc886f8018695a0a18d7b80e3
Pranjali <pranjali@Pranjalis-MacBook-Air.local>
2025-05-09 13:39:09 +0530

    modifying folder name

---
+++ jenkins/README.md
@@ -0,0 +1,2 @@
+# terraform-jenkins-eks
+Deploying an EKS cluster with Terraform and Jenkins.
---
+++ jenkins/data.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "amazon_linux_2" {
+
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_availability_zones" "azs" {}
